21a63b1b198191c04ac09fa6eb4f6c65e8549029
Leo Dirac <leo@scaledrecognition.com>
2013-08-19 13:15:07 -0700

    Initial blank RoR app w/o activerecord.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codetrendror::Application.config.secret_token = '17b9c0cd7d72f3a65c356b05d185b21547d812875af50a10282764df8eced82ace6e04e2b130a08edd3d799184376df28e49a5476eac5aaa734db825c572c1ed'
