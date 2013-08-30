ea22a9127ca506bca0a2499f69a979990133809d
Leo Dirac <leo@scaledrecognition.com>
2013-08-30 12:27:35 -0700

    Deals with trailing whitespace in search queries.

--- app/controllers/technologies_controller.rb
+++ app/controllers/technologies_controller.rb
@@ -86,7 +86,7 @@ class TechnologiesController < ApplicationController
 
   def search
     #TODO: This search needs to be totally redone to include stemming and partials, etc.
-    @query = params[:query]
+    @query = params[:query].strip!
     @technology = Technology.find_by(techtag: @query)
     if @technology
         # We found it!
